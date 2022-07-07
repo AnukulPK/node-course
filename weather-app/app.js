709f669afcb18ed87166e7c849550b63ca29c5b3
AnukulPK <anukulpk2@gmail.com>
2022-07-07 09:57:58 +0530

    http request for weather details

--- weather-app/app.js
+++ weather-app/app.js
@@ -1,11 +1,9 @@
-console.log('Starting');
+const request = require('request');
 
-setTimeout(() => {
-  console.log('2 second timer');
-}, 2000);
+const url =
+  'http://api.weatherstack.com/current?access_key=a7a64df4097f538f958aec8f033c7299&query=37.82.67,-122.4233';
 
-setTimeout(() => {
-  console.log('0 second timer');
-}, 0);
-
-console.log('Stopping');
+request({ url: url }, (error, response) => {
+  const data = JSON.parse(response.body);
+  console.log(data.current);
+});
