31bb7cd18ae97c9bc5f9f0c2390f2c7bb285a158
unknown <anukulpk2@gmail.com>
2022-07-08 21:29:55 +0530

    added api response

--- weather-app/app.js
+++ weather-app/app.js
@@ -1,9 +1,11 @@
-const request = require('request');
+const request = require("request");
 
 const url =
-  'http://api.weatherstack.com/current?access_key=a7a64df4097f538f958aec8f033c7299&query=37.82.67,-122.4233';
+  "http://api.weatherstack.com/current?access_key=a7a64df4097f538f958aec8f033c7299&query=37.82.67,-122.4233&units=f";
 
-request({ url: url }, (error, response) => {
-  const data = JSON.parse(response.body);
-  console.log(data.current);
+request({ url: url, json: true }, (_error, response) => {
+  const data = response.body;
+  console.log(
+    `${data.current.weather_descriptions[0]}. Is is currently ${data.current.temperature} degrees out. It feels like ${data.current.feelslike} degrees out`
+  );
 });
