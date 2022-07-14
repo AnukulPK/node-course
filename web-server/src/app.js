623e71576c638f50d1e4336d02d653fcacd5e1f8
AnukulPK <anukulpk2@gmail.com>
2022-07-14 10:18:21 +0530

    serving html pages

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -1,22 +1,38 @@
+const path = require("path");
 const express = require("express");
 
 const app = express();
+const publicDirectoryPath = path.join(__dirname, "../public");
+
+app.use(express.static(publicDirectoryPath));
 
 app.get("/weather", (req, res) => {
-  res.send("Show Weather!");
+  res.send({
+    location: "Bangalore",
+    forecast: "It is raining",
+  });
 });
 
-app.get("/about", (req, res) => {
-  res.send("About Page!");
-});
+// app.get("/about", (req, res) => {
+//   res.send("<h1>About Page!</h1>");
+// });
 
-app.get("/help", (req, res) => {
-  res.send("Help Page!");
-});
+// app.get("/help", (req, res) => {
+//   res.send([
+//     {
+//       name: "Anukul",
+//       age: 27,
+//     },
+//     {
+//       name: "test",
+//       age: 26,
+//     },
+//   ]);
+// });
 
-app.get("", (request, response) => {
-  response.send("Hello Express!");
-});
+// app.get("", (request, response) => {
+//   response.send("<h1>Weather</h1>");
+// });
 
 app.listen(3000, () => {
   console.log("Server is up and runinng on Port 3000");
