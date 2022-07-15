a72f1b9b5a4f529e13795bf5916b1b9a77af5893
AnukulPK <anukulpk2@gmail.com>
2022-07-15 10:14:39 +0530

    added views

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -4,7 +4,28 @@ const express = require("express");
 const app = express();
 const publicDirectoryPath = path.join(__dirname, "../public");
 
+app.set("view engine", "hbs");
 app.use(express.static(publicDirectoryPath));
+
+app.get("", (req, res) => {
+  res.render("index", {
+    title: "Weather",
+    name: "Anukul PK",
+  });
+});
+
+app.get("/about", (req, res) => {
+  res.render("about", {
+    title: "About Me",
+    name: "Anukul PK",
+  });
+});
+
+app.get("/help", (req, res) => {
+  res.render("help", {
+    helpText: "Please find help here",
+  });
+});
 
 app.get("/weather", (req, res) => {
   res.send({
